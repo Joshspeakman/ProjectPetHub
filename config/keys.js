b95d9eb544bd63c48f679eee2af7733ea4027a6e
Joshua Speakman <44591690+Joshspeakman@users.noreply.github.com>
2019-05-02 21:48:21 -0600

    working on keys

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     mongoURI: `mongodb://pethub1:lucky0ne@ds151086.mlab.com:51086/heroku_gx9zbbp0`,
-    mongoURI2: `mongodb://heroku_gx9zbbp0:lucky0ne@ds151086.mlab.com:51086/heroku_gx9zbbp0',
+    //mongoURI2: `mongodb://heroku_gx9zbbp0:lucky0ne@ds151086.mlab.com:51086/heroku_gx9zbbp0',
     //mongoURI3: ENV['MONGODB_URI'],
     mongoURILocal: `mongodb://localhost/pethub`,
     secretOrKey: "secret"
   };
 
-  heroku_gx9zbbp0
+  
